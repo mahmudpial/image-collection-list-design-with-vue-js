e903e49b1fbfe42c35b87408306189de7cb43ab8
pial <pialmahmud80@gmail.com>
2026-02-07 15:59:22 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="circle-one"></div>
+    <div class="circle-two"></div>
+
+    <div class="container">
+        <h1>Popular Collections</h1>
+        <ul class="categories">
+            <li>Profile</li>
+            <li>New York</li>
+            <li>Relaxing</li>
+            <li>Person</li>
+            <li>Fashion</li>
+        </ul>
+        <div class="inner-container">
+            <section class="people">
+                <img
+                    src="/images/people/people1.jpg"
+                    alt="woman face"
+                >
+                <div class="images">
+                    <img
+                        src="/images/people/people2.jpg"
+                        alt="woman face"
+                    >
+                    <img
+                        src="/images/people/people3.jpg"
+                        alt="woman face"
+                    >
+                    <img
+                        src="/images/people/people4.jpg"
+                        alt="woman face"
+                    >
+                </div>
+                <div class="pic-number">
+                    <h3>People</h3>
+                    <div class="num">
+                        <i class="fa-regular fa-image"></i>
+                        <p>144</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="nature">
+                <img
+                    src="/images/nature/nature_1.jpg"
+                    alt="nature"
+                >
+                <div class="images">
+                    <img
+                        src="/images/nature/nature_2.jpg"
+                        alt="nature"
+                    >
+                    <img
+                        src="/images/nature/nature_3.jpg"
+                        alt="nature"
+                    >
+                    <img
+                        src="/images/nature/nature_4.jpg"
+                        alt="nature"
+                    >
+                </div>
+                <div class="pic-number">
+                    <h3>Nature</h3>
+                    <div class="num">
+                        <i class="fa-regular fa-image"></i>
+                        <p>7K</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="history">
+                <img
+                    src="/images/history/history1.jpg"
+                    alt="history"
+                >
+                <div class="images">
+                    <img
+                        src="/images/history/history2.jpg"
+                        alt="history"
+                    >
+                    <img
+                        src="/images/history/history3.jpg"
+                        alt="history"
+                    >
+                    <img
+                        src="/images/history/history4.jpg"
+                        alt="history"
+                    >
+                    
+                </div>
+                <div class="pic-number">
+                    <h3>History</h3>
+                    <div class="num">
+                        <i class="fa-regular fa-image"></i>
+                        <p>431</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div> 
+</template>
+
+<!--This is a Vue 3 Single File Component (SFC) structure.
+The <template> section contains the HTML markup for the component,
+while the <script setup> section is where you can write your JavaScript logic using the Composition API.
+-->
+
+<script setup>
+
+
+</script>
+
+<!--The <style scoped> section is for writing CSS that will only apply to this component,
+preventing styles from leaking out to other parts of the application. -->
+<style scoped></style>
