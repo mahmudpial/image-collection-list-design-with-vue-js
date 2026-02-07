b3a7e3354ea52faf942c70da486b4028326c6167
pial <pialmahmud80@gmail.com>
2026-02-07 16:59:42 +0600

    config git page

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  // For GitHub Pages deployment, add the following line to set the base path:
+  base: "/https://mahmudpial.github.io/image-collection-list-design-with-vue-js/", // ← replace with your GitHub repo name
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
